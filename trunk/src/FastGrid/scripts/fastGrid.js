956f5d0c04c06fd09647996753a5aeea6dc982cd
Alexsei Matvienko <alexsei_matvienko@epam.com>
2015-01-23 20:06:42 +0300

    initial commit

---
+++ trunk/src/FastGrid/scripts/fastGrid.js
@@ -0,0 +1,22 @@
+﻿fg = {};
+
+fg.fastGrid = function(options) {
+    this.$canvas = $('#' + options.id);
+    this.columns = options.columns;
+    this.rows = options.rows;
+};
+
+fg.fastGrid.prototype.id = undefined;
+
+/**
+* Jquery object
+*/
+fg.fastGrid.prototype.$canvas = undefined;
+
+fg.fastGrid.prototype.columns = undefined;
+
+fg.fastGrid.prototype.rows = undefined;
+
+fg.fastGrid.prototype.render = function () {
+
+};
