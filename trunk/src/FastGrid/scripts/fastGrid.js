aa7620f6b27ad7fc857f458d3c3d8b7d070dd8b1
Alexsei Matvienko <alexsei_matvienko@epam.com>
2015-01-23 20:18:48 +0300

    further development

--- trunk/src/FastGrid/scripts/fastGrid.js
+++ trunk/src/FastGrid/scripts/fastGrid.js
@@ -1,7 +1,7 @@
-﻿fg = {};
+﻿fg = fg || {};
 
 fg.fastGrid = function(options) {
-    this.$canvas = $('#' + options.id);
+    this.canvas = document.getElementById(options.id);
     this.columns = options.columns;
     this.rows = options.rows;
 };
@@ -11,12 +11,21 @@ fg.fastGrid.prototype.id = undefined;
 /**
 * Jquery object
 */
-fg.fastGrid.prototype.$canvas = undefined;
+fg.fastGrid.prototype.canvas = undefined;
 
 fg.fastGrid.prototype.columns = undefined;
 
 fg.fastGrid.prototype.rows = undefined;
 
-fg.fastGrid.prototype.render = function () {
+fg.fastGrid.prototype.render = function() {
+    if (!this.canvasContext_) {
+        this.canvasContext_ = this.canvas.getContext('2d');
+    }
 
+    for (var columnIndex = 0, columnsLenght = this.columns; columnIndex < columnsLenght; columnIndex++) {
+        
+    }
+
+    this.canvasContext_.fillStyle = '#FF0000';
+    this.canvasContext_.fillRect(0, 0, 150, 75);
 };
